5b602813e2b7908af9041b3a26c16d40fb17c155
drewverlee <drew.verlee@gmail.com>
2021-07-25 10:54:36 -0500

    add `optimal-move` function.
    Similar to valid-move, optimal-move finds the a next move to play in the game of tic-tac-toe
    and returns that added to the end of list of existing moves.
    
    It has 5 steps. win, don't lose, pick center, pick corner, pick any.
    
    Also we opened up the play game function to take a strategy e.g optimal-move.

--- src/toehold/core.clj
+++ src/toehold/core.clj
@@ -2,7 +2,8 @@
   (:require [clojure.core.logic :as l]
             [clojure.core.logic.arithmetic :as la]
             [clojure.pprint :refer [pprint]]
-            [toehold.utils :refer [inspect]])
+            [toehold.utils :refer [inspect]]
+            [clojure.set :as set])
   (:gen-class))
 
 (def players #{:x :o})
@@ -40,14 +41,15 @@
 
 (defn- rand-player [] (rand-nth players))
 
-(defn- rand-valid-move [moves & [player]]
-  (let [board (board-from moves) ; TODO memoize?
+(defn rand-valid-move [moves & [player]]
+  (let [board     (board-from moves) ; TODO memoize?
         avl-moves (available-moves board)]
     (assert (seq avl-moves) ; Make sure board's not full
             (str "No valid moves left on " board))
     (conj moves
           (conj (rand-nth avl-moves)
                 (or player (cur-player moves))))))
+
 
 (defn full? [moves]
   (>= (count moves) 9))
@@ -79,25 +81,58 @@ we do the same thing but for [2 1 0]."
     (when (every? #(= % one) triplet)
       one)))
 
+(defn full-or-win? [moves]
+  (or (full? moves) (win? moves)))
+
 (defn win?
   "Given a list of moves, return the winning player (or nil if none)"
   [moves]
   (first (keep check-triplet (triplets (board-from moves)))))
 
-(defn full-or-win? [moves]
-  (or (full? moves) (win? moves)))
+(defn optimal-move [moves & [player]]
+  (let [board                       (board-from moves) ; TODO memoize?
+        avl-moves                   (available-moves board)
+        player                      (or player (cur-player moves))
+        other-player                ({:x :o :o :x} player)
+        possible-player-moves       (map #(conj % player) avl-moves)
+        possible-other-player-moves (map #(conj % other-player) avl-moves)]
+    (assert (seq avl-moves) ; Make sure board's not full
+            (str "No valid moves left on " board))
+    (conj moves
+          (some #(when (seq %) (first (shuffle %)))
+                ;; win the game
+                [(filter #(win? (conj moves % )) possible-player-moves)
 
-(defn rand-game
-  "Return a game consisting of a sequence of valid moves ending in a win
-  or a full board"
-  []
+                 ;; don't lose the game
+                 ;; check what move would let the other player win and take it
+                 (->> possible-other-player-moves
+                      (filter #(win? (conj moves %)))
+                      (map (fn [[x y]] [x y player])))
+
+                 ;; From here you take spaces based on how many triplets they touch the center touches the most
+                 ;; and is critical that it's take by the second player if they don't want to lose to another optimal player.
+
+                 ;; take center
+                 (set/intersection (set possible-player-moves) #{[1 1 player]})
+
+                 ;; take corners
+                 (set/intersection (set possible-player-moves) #{[0 0 player]
+                                                                 [0 2 player]
+                                                                 [2 0 player]
+                                                                 [2 2 player]})
+
+                 ;; take whatever
+                 possible-player-moves]))))
+
+(defn stragety->game-output
+  [stragety]
   (first (drop-while (comp not full-or-win?)
-                     (iterate rand-valid-move []))))
+                     (iterate stragety []))))
 
 (defn -main
   "Minimal usage example"
   [& args]
-  (let [game (rand-game)]
+  (let [game (stragety->game-output rand-valid-move)]
     (println "moves: " game)
     (println "board at end of random game: " (board-from game))
     (println "winner: " (win? game))
