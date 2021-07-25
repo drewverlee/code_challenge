da95dabd5ee5a670a65f6382b2ea7dd207b03b5d
drewverlee <drew.verlee@gmail.com>
2021-07-25 10:45:28 -0500

    bump clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.logic "0.8.10"]
                  [pjstadig/humane-test-output "0.8.0"]
                  [org.clojure/math.combinatorics "0.1.6"]
