c2bb7619f0e10f94ab19086e192fd372d776976f
drewverlee <drew.verlee@gmail.com>
2021-07-26 17:15:15 -0500

    remove code from tree ns that we moved to analytics

--- src/toehold/tree.clj
+++ src/toehold/tree.clj
@@ -52,44 +52,6 @@
 ;; It then just has to validate that set of moves. I did a rough count in my head and it took around 40 seconds in my nrepl, but I suspect that could be halved by multithreaded solution.
 ;; In part I did it this way first because I'm not sure how to do a significant improve on the my next solution which will build the tree and try to memoize paths we have already seen.
 
-;; I didn't use the content-zipper or any existing code here because I went with a treeless solution.
-;; NOTE i'm mildly worried the total valid end of game moves is off, something concerning the last set of permutations
-(comment
-  (let [moves->valid-end-of-game?
-        (fn [moves]
-          "moves is an a collection of alternating player owned spots, return truthy value if the moves form a valid end of game."
-          (let [player-spots->winning-sets
-                (fn
-                  [player-owned-spots]
-                  (filter
-                    #(set/subset? % (set player-owned-spots))
-                    ;; board for reference
-                    ;; 0 1 2
-                    ;; 3 4 5
-                    ;; 6 7 8
-                    #{#{0 1 2} #{3 4 5} #{6 7 8}
-                      #{0 3 6} #{1 4 7} #{2 5 8}
-                      #{0 4 8} #{2 4 6}}))
-                winning-sets (concat
-                               (player-spots->winning-sets (take-nth 2 moves))
-                               (player-spots->winning-sets (take-nth 2 (rest moves))))
-                winning-set  (first winning-sets)
-                draw?        (and
-                               (zero? (count winning-sets))
-                               (= 9 (count moves)))
-                one-winner?  (and
-                               ;; one winner
-                               (= 1 (count winning-sets))
-
-                               ;; last move won it
-                               (winning-set (last moves)))]
-            (or draw? one-winner?))) ]
-
-    (->> (range 5 10)
-         (mapcat #(combo/permuted-combinations (range 9) %))
-         (filter moves->valid-end-of-game?)
-         count))
-  ;; => 242496
 
   )
 
