46d1f4cea6914ba834987f601c2c39a7a44ff867
drewverlee <drew.verlee@gmail.com>
2021-07-26 15:33:01 -0500

    move permutations answer to analytics

--- src/toehold/analytics.clj
+++ src/toehold/analytics.clj
@@ -1,6 +1,63 @@
 (ns toehold.analytics
   (:require [toehold.core :as ttt]
-            [clojure.test :as t]))
+            [clojure.test :as t]
+            [clojure.set :as set]
+            [clojure.math.combinatorics :as combo]))
+
+
+
+;; CHALLENGE 2: Write a function to build a tree of all possible games. Explain
+;; why or why not it uses content-zipper (above).
+
+;; Part one:  Brute force / treeless for generating all valid game states
+;;
+;; I wanted to sketch something out first that would find all the possible games using a more brute force approach. This creates all permutations for a series of player moves (spots claimed) of sizes 5 and up to and including 9.
+;; It then just has to validate that set of moves. I did a rough count in my head and it took around 40 seconds in my nrepl, but I suspect that could be halved by multithreaded solution.
+;; In part I did it this way first because I'm not sure how to do a significant improve on the my next solution which will build the tree and try to memoize paths we have already seen.
+
+;; I didn't use the content-zipper or any existing code here because I went with a treeless solution.
+;; NOTE i'm mildly worried the total valid end of game moves is off, something concerning the last set of permutations
+(comment
+  (let [moves->valid-end-of-game?
+        (fn [moves]
+          "moves is an a collection of alternating player owned spots, return truthy value if the moves form a valid end of game."
+          (let [player-spots->winning-sets
+                (fn
+                  [player-owned-spots]
+                  (filter
+                    #(set/subset? % (set player-owned-spots))
+                    ;; board for reference
+                    ;; 0 1 2
+                    ;; 3 4 5
+                    ;; 6 7 8
+
+                    #{#{0 1 2} #{3 4 5} #{6 7 8}
+                      #{0 3 6} #{1 4 7} #{2 5 8}
+                      #{0 4 8} #{2 4 6}}))
+                winning-sets (concat
+                               (player-spots->winning-sets (take-nth 2 moves))
+                               (player-spots->winning-sets (take-nth 2 (rest moves))))
+                winning-set  (first winning-sets)
+                draw?        (and
+                               (zero? (count winning-sets))
+                               (= 9 (count moves)))
+                one-winner?  (and
+                               ;; one winner
+                               (= 1 (count winning-sets))
+
+                               ;; last move won it
+                               (winning-set (last moves)))]
+            (or draw? one-winner?))) ]
+
+    (->> (range 5 10)
+         (mapcat #(combo/permuted-combinations (range 9) %))
+         (filter moves->valid-end-of-game?)
+         count))
+  ;; => 242496
+  ;; I think should should be 255168 based off http://www.se16.info/hgb/tictactoe.htm
+  ;; the 9th permutation returns less then it should.
+  )
+
 
 ;; Questions overview
 ;; CHALLENGE 4: write code to answer some of the following questions:
